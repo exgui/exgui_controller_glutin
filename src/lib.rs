d044b3cb910e53cc4e3ba7cbf3d38b89b6979c61
XX <freecoder.xx@gmail.com>
2019-01-23 22:05:14 +0300

    Add exit by escape and AppState

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,16 @@ use glutin::{WindowBuilder, ContextBuilder, EventsLoop, GlWindow, GlContext, Ele
 use derive_more::From as FromMode;
 use exgui::{Comp, Color, controller::MouseInput};
 
+pub enum AppState {
+    Exit,
+    Continue,
+}
+
 pub struct App {
     events_loop: Option<EventsLoop>,
     window: GlWindow,
     background_color: Color,
+    exit_by_escape: bool,
     width: u32,
     height: u32,
 }
@@ -39,11 +45,17 @@ impl App {
             background_color: Color::RGBA(0.8, 0.8, 0.8, 1.0),
             width,
             height,
+            exit_by_escape: true,
         })
     }
 
     pub fn with_background_color(mut self, color: Color) -> Self {
         self.background_color = color;
+        self
+    }
+
+    pub fn with_exit_by_escape(mut self, exit: bool) -> Self {
+        self.exit_by_escape = exit;
         self
     }
 
@@ -59,10 +71,12 @@ impl App {
 
     #[inline]
     pub fn run(&mut self, comp: &mut Comp) -> Result<(), AppError> {
-        self.run_proc(comp, |_, _| {})
+        self.run_proc(comp, |_, _| AppState::Continue)
     }
 
-    pub fn run_proc(&mut self, comp: &mut Comp, mut proc: impl FnMut(&App, &mut Comp)) -> Result<(), AppError> {
+    pub fn run_proc(&mut self, comp: &mut Comp, mut proc: impl FnMut(&App, &mut Comp) -> AppState)
+        -> Result<(), AppError>
+    {
         let mut mouse_controller = MouseInput::new();
         let mut events_loop = mem::replace(&mut self.events_loop, None)
             .ok_or(AppError::EventsLoopIsNone)?;
@@ -79,14 +93,23 @@ impl App {
                 );
             }
 
-            proc(self, comp);
+            if let AppState::Exit = proc(self, comp) {
+                break;
+            }
 
             self.window.swap_buffers()?;
 
             events_loop.poll_events(|event| match event {
                 glutin::Event::WindowEvent { event, .. } => {
                     match event {
-                        glutin::WindowEvent::Closed => running = false,
+                        glutin::WindowEvent::Closed  => running = false,
+                        glutin::WindowEvent::KeyboardInput {
+                            input: glutin::KeyboardInput {
+                                virtual_keycode: Some(glutin::VirtualKeyCode::Escape),
+                                ..
+                            },
+                            ..
+                        } if self.exit_by_escape => running = false,
                         glutin::WindowEvent::Resized(w, h) => self.window.resize(w, h),
                         glutin::WindowEvent::CursorMoved { position: (x_pos, y_pos), .. } => {
                             mouse_controller.update_pos(x_pos, y_pos);
